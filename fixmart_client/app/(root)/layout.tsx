50239e6aee04d2d6aa61d4dd2e36a610bb7522ce
Nuthan B <NuthanB@protonmail.com>
2024-04-13 04:36:30 +0530

    Added the login and sign up

---
+++ fixmart_client/app/(root)/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "../globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Fixmart store",
+  description: "A store for all things",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ClerkProvider>{children}</ClerkProvider></body>
+    </html>
+  );
+}
---
+++ fixmart_client/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <>
+     Home Page
+    </>
+  );
+}
+
+export const dynamic = "force-dynamic";
