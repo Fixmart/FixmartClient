a31a8b67d78a20077b9f3c76861fe405147be399
pragati0103 <pragatikomarla@gmail.com>
2024-04-16 00:58:52 +0530

    changes

--- fixmart_client/app/(root)/cart/page.tsx
+++ fixmart_client/app/(root)/cart/page.tsx
@@ -2,33 +2,34 @@ import React from 'react'
 import { MinusCircle,  PlusCircle,  Trash } from 'lucide-react';
 import useCart from '@/lib/hooks/useCart';
 import Image from 'next/image';
-
 const Cart = () => {
   const cart= useCart()
 
-  const total= cart.cartItems.reduce((acc,cartItem) => acc+ cartItem.item.price * cartItem.quantity,0)
-  const totalRounded= parseFloat(total.toFixed(2))
+  const total= cart.cartItems.reduce(
+    (acc,cartItem) => acc+ cartItem.item.price * cartItem.quantity,0);
+
+  const totalRounded= parseFloat(total.toFixed(2));
 
 
   return (
     <div className= 'flex gap-20 py-16 px-10 max-lg:flex-col'>
       <div className='w-2/3 max-lg:w-full'>
          <p className='text-handing3-bold'>Shopping Cart</p>
-         <hr className="my06"/>
+         <hr className="my-6"/>
          {cart.cartItems.length ===0 ? (
           <p className='text-body-bold'> No item in cart </p>
          ): (
           <div>
-           {cart.cartItems.map((cartItem) => (
-              <div className='w-full flex max-sm:flex-col max-sm:gap-3 hover:bg-grey-1 px-6 py-5 max-sm:items-start justify-between items-center'>
-                <div className='flex items-center'>
-
-                <Image
-                  src={cartItem.item.media[0]} 
-                  width={100} 
-                  height={100} 
-                  className="rounded-lg w-32 h-32 object-cover"
-                  alt='product '
+            {cart.cartItems.map((cartItem) => (
+               <div className="w-full flex max-sm:flex-col max-sm:gap-3 hover:bg-grey-1 px-4 py-3 items-center max-sm:items-start justify-between">
+               <div className="flex items-center">
+                 
+                 <Image
+                   src={cartItem.item.media[0]}
+                   width={100}
+                   height={100}
+                   className="rounded-lg w-32 h-32 object-cover"
+                   alt="product"
                  />
 
                  <div className='flex flex gap-3 ml-4'>
@@ -58,8 +59,9 @@ const Cart = () => {
 
               </div>
 
-              <Trash className="hover:text-red-1 cursor-pointer" 
-                  onClick={()=>cart.removeItem(cartItem.item._id)}
+              <Trash 
+                  className="hover:text-red-1 cursor-pointer" 
+                  onClick={() => cart.removeItem(cartItem.item._id)}
                 />
 
               </div>
@@ -71,7 +73,9 @@ const Cart = () => {
 
       <div className="w-1/3 max-lg:w-full flex flex-col gap-8 bg-grey-1 rounded-1g px-4 py-5">
         <p className='text-heading4-bold pb-4'>
-          Summary <span> {` ($(cart.cartItems.length} $(cart.cartItems.length> 1? "items":"item"}) `}</span>
+          Summary <span> {`(${cart.cartItems.length} ${
+            cart.cartItems.length > 1 ? "items" : "item"
+          })`}</span>
         </p>
         <div className='flex justify-between text-body-semibold'>
            <span>Total Amount </span>
@@ -82,7 +86,7 @@ const Cart = () => {
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Cart
