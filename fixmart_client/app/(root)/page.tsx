a012bafb7f8ac1bbfa3118d79d9b901c36365f81
AnkithaKoushik <121386672+AnkithaKoushik@users.noreply.github.com>
2024-04-20 10:14:57 +0530

    Font-added

--- fixmart_client/app/(root)/page.tsx
+++ fixmart_client/app/(root)/page.tsx
@@ -1,13 +1,21 @@
 import Collections from "@/components/Collections";
 import ProductList from "@/components/ProductList";
 import Image from "next/image";
+import { Playfair_Display } from 'next/font/google';
+
+const playfair_display = Playfair_Display({
+  subsets: ['cyrillic'],
+  weight: "700"
+});
 
 export default function Home() {
   return (
     <>
-      <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
-      <Collections/>
-      <ProductList/>
+      <main className={playfair_display.className}>  
+        <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
+        <Collections/>
+        <ProductList/>
+      </main>
     </>
   );
 }
