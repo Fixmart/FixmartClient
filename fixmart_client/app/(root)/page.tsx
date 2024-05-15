51cb37fbba485555428b5deb2d33138512b31bca
AnkithaKoushik <121386672+AnkithaKoushik@users.noreply.github.com>
2024-05-16 02:16:03 +0530

    Font change

--- fixmart_client/app/(root)/page.tsx
+++ fixmart_client/app/(root)/page.tsx
@@ -1,17 +1,17 @@
 import Collections from "@/components/Collections";
 import ProductList from "@/components/ProductList";
 import Image from "next/image";
-import { Playfair_Display } from 'next/font/google';
+import { Roboto_Slab } from 'next/font/google';
 
-const playfair_display = Playfair_Display({
+const roboto_Slab = Roboto_Slab({
   subsets: ['cyrillic'],
-  weight: "700"
+  weight: "300"
 });
 
 export default function Home() {
   return (
     <>
-      <main className={playfair_display.className}>  
+      <main className={roboto_Slab.className}>  
         <Image src="/banner.png" alt="banner" width={2000} height={1000} className="w-screen" />
         <Collections/>
         <ProductList/>
